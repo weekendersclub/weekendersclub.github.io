bc3eba28cbfd9784e34c0c22e25c8cb048c34f72
wawanzs <syzwnzhr@gmail.com>
2014-08-26 23:24:26 +0800

    added bootstrap

--- js/goweekenders.js
+++ js/goweekenders.js
@@ -5,22 +5,27 @@ jQuery(document).ready(function($) {
         'responsive' : true,
         'automatic' : true,
         'animduration' : 450,
-        'animspeed' : 4000,
+        'animspeed' : 6000,
         'nexttext' : '<span class="fa fa-chevron-right"></span>',
         'prevtext' : '<span class="fa fa-chevron-left"></span>',
         'hoverpause' : false
     });
 });
 
-$(document).ready(function() {
+function recalcSlider() {
 	$('#wcSplash').css({
 		'height' : ($(window).height()) - 50,
 		'width' : ($(window).width())
 	});
+	$('#wcSlide').css({
+		'height' : ($(window).height()) - 50,
+		'width' : ($(window).width())
+	});
+}
+
+$(document).ready(function() {
+	recalcSlider();
 });
 $(window).resize(function() {
-	$('#wcSplash').css({
-		'height' : ($(window).height()) - 50,
-		'width' : ($(window).width())
-	});
+	recalcSlider();
 });
